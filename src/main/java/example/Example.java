04fa9aa79f13d29175fc6a02143d0a3b3ad0aa0c
justinas.krutulis <justinaskrutulis@gmail.com>
2018-02-11 22:33:00 +0200

    SQ integration

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -3,9 +3,11 @@ package example;
 import com.google.common.base.Joiner;
 
 public class Example {
+    private Example() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static String join(String... args) {
         return Joiner.on(' ').join(args);
     }
-
 }
